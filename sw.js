930af94d17e1077b19c59c89c9429fffb5731b38
Bot <y@y.com>
2026-04-17 08:19:34 +0000

    fix: bottom nav visible + mod layout fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v9';
+const CACHE='yeshiva-v10';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
