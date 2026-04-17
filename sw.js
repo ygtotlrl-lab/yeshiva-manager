f85b03aa1b2f9c2530d76b25e2a30bb356c23c78
Bot <y@y.com>
2026-04-17 07:54:11 +0000

    mobile-first CSS - default mobile, desktop only above 1100px

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v5';
+const CACHE='yeshiva-v6';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
