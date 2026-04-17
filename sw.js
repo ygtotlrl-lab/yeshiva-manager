07e3a66af5d11eb859f3f8aed0540e5c6deb020d
Bot <y@y.com>
2026-04-17 08:17:34 +0000

    mobile: centered layout, bigger text, visible bottom nav 2 rows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yeshiva-v8';
+const CACHE='yeshiva-v9';
 const ASSETS=['/yeshiva-manager/','/yeshiva-manager/index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
